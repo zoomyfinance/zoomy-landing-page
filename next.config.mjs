98915a7cef1e052d9954a21e78c27cefb0a3a6a4
Silver <silverg33k@gmail.com>
2025-03-04 19:54:18 +0100

    chore: changed next config file to mjs

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'export', // Enable static export
+}
+
+export default nextConfig;
